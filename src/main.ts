a645d077b3a2fe58da0f3fe741d54c65fa3ba871
jeorregog <jeorregog1@eafit.edu.co>
2026-09-05 18:21:29 -0500

    fix: initialize seed data on app startup

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/styles/main.css'
 import router from './router'
+import { InitializationService } from './services/InitializationService'
+
+InitializationService.initialize()
 
 const app = createApp(App)
 
